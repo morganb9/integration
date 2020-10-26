bccfbd47d2c35a6b72338f4f0cc4ec03943142c7
morganb9 <43564973+morganb9@users.noreply.github.com>
2020-10-26 11:17:24 +0100

    Update Operation.java
    yo la street

--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -2,6 +2,12 @@ public class Operation {
 
     public Operation(){}
 
+    /**
+     * Yo
+     * @param a
+     * @param b
+     * @return
+     */
     public static int addition(int a, int b){
         return a+b;
     }
